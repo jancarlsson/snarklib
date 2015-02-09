6e7790bbd4eaf170d73e202afa69cabf63e434a0
jancarlsson <jancarlsson@Safe-mail.net>
2015-02-09 20:15:51 +0000

    H subsumes combined ABC witness

--- QAP_witness.hpp
+++ QAP_witness.hpp
@@ -14,7 +14,7 @@ namespace snarklib {
 // witness vectors A, B, C
 //
 
-template <template <typename> class SYS, typename T, char R1C>
+template <template <typename> class SYS, typename T>
 class QAP_WitnessABC
 {
 public:
@@ -22,38 +22,46 @@ public:
                    const R1Witness<T>& witness)
         : m_qap(qap),
           m_witness(witness),
-          m_vec(qap.degree(), T::zero()),
-          m_uit(m_vec.begin()),
+          m_vecA(qap.degree(), T::zero()),
+          m_vecB(qap.degree(), T::zero()),
+          m_vecC(qap.degree(), T::zero()),
+          m_uitA(m_vecA.begin()),
+          m_uitB(m_vecB.begin()),
+          m_uitC(m_vecC.begin()),
           m_error(false)
     {
-        // input consistency
-        switch (R1C) {
-        case ('a') :
-        case ('A') :
-            *m_uit = T::one();
-            for (std::size_t i = 0; i < qap.numCircuitInputs(); ++i) {
-                *m_uit += witness[i] * T(i + 2);
-            }
+        // input consistency (for A only)
+        *m_uitA = T::one();
+        for (std::size_t i = 0; i < qap.numCircuitInputs(); ++i) {
+            *m_uitA += witness[i] * T(i + 2);
         }
 
+        // A, B, C
         constraintLoop(qap.constraintSystem());
 
-        qap.FFT()->iFFT(m_vec);
+        qap.FFT()->iFFT(m_vecA);
+        qap.FFT()->iFFT(m_vecB);
+        qap.FFT()->iFFT(m_vecC);
     }
 
     void cosetFFT() {
-        m_qap.FFT()->cosetFFT(m_vec, T::params.multiplicative_generator());
+        m_qap.FFT()->cosetFFT(m_vecA, T::params.multiplicative_generator());
+        m_qap.FFT()->cosetFFT(m_vecB, T::params.multiplicative_generator());
+        m_qap.FFT()->cosetFFT(m_vecC, T::params.multiplicative_generator());
     }
 
-    const std::vector<T>& vec() const { return m_vec; }
+    const std::vector<T>& vecA() const { return m_vecA; }
+    const std::vector<T>& vecB() const { return m_vecB; }
+    const std::vector<T>& vecC() const { return m_vecC; }
 
     bool operator! () const { return m_error; }
 
 private:
     void constraintLoop(const R1System<T>& S) {
         for (const auto& constraint : S.constraints()) {
-            ++m_uit;
-            *m_uit += constraint.combo(R1C) * m_witness;
+            ++m_uitA; *m_uitA += constraint.a() * m_witness;
+            ++m_uitB; *m_uitB += constraint.b() * m_witness;
+            ++m_uitC; *m_uitC += constraint.c() * m_witness;
         }
     }
 
@@ -67,128 +75,65 @@ private:
 
     const QAP_SystemPoint<SYS, T>& m_qap;
     const R1Witness<T>& m_witness;
-    std::vector<T> m_vec;
-    typename std::vector<T>::iterator m_uit;
+    std::vector<T> m_vecA, m_vecB, m_vecC;
+    typename std::vector<T>::iterator m_uitA, m_uitB, m_uitC;
     bool m_error;
 };
 
-template <template <typename> class SYS, typename T> using
-QAP_WitnessA = QAP_WitnessABC<SYS, T, 'A'>;
-
-template <template <typename> class SYS, typename T> using
-QAP_WitnessB = QAP_WitnessABC<SYS, T, 'B'>;
-
-template <template <typename> class SYS, typename T> using
-QAP_WitnessC = QAP_WitnessABC<SYS, T, 'C'>;
-
 ////////////////////////////////////////////////////////////////////////////////
-// witness vector H
+// witness vector H subsumes ABC
 //
 
 template <template <typename> class SYS, typename T>
-class QAP_WitnessH
+class QAP_WitnessABCH
 {
 public:
-    ////////////////////////////////////////
-    // regular H
-    //
-
-    QAP_WitnessH(const QAP_SystemPoint<SYS, T>& qap,
-                 const QAP_WitnessA<SYS, T>& aA, // before cosetFFT()
-                 const QAP_WitnessB<SYS, T>& aB, // before cosetFFT()
-                 const T& random_d1,
-                 const T& random_d2,
-                 const T& random_d3)
-        : QAP_WitnessH{qap, random_d1, random_d2, random_d3}
+    QAP_WitnessABCH(const QAP_SystemPoint<SYS, T>& qap,
+                    const R1Witness<T>& witness,
+                    const T& random_d1,
+                    const T& random_d2,
+                    const T& random_d3)
+        : m_vec(qap.degree() + 1, T::zero())
     {
+        QAP_WitnessABC<SYS, T> ABC(qap, witness);
+
         for (std::size_t i = 0; i < qap.degree(); ++i)
-            m_vec[i] = random_d2 * aA.vec()[i] + random_d1 * aB.vec()[i];
+            m_vec[i] =
+                random_d2 * ABC.vecA()[i] +
+                random_d1 * ABC.vecB()[i];
 
         m_vec[0] -= random_d3;
-        qap.FFT()->add_poly_Z(random_d1 * random_d2, m_vec);
+
+        qap.FFT()->add_poly_Z(random_d1 * random_d2,
+                              m_vec);
+
+        ABC.cosetFFT();
+
+        addTemporary(
+            QAP_WitnessABCH<SYS, T>(qap, ABC));
     }
 
-    // use with accumVector() to avoid having all vectors in memory
-    QAP_WitnessH(const QAP_SystemPoint<SYS, T>& qap,
-                 const T& random_d1,
-                 const T& random_d2,
-                 const T& random_d3)
-        : m_vec(qap.degree() + 1, T::zero()),
-          m_blockCount(0),
-          m_qap(qap),
-          m_random_d1(random_d1),
-          m_random_d2(random_d2),
-          m_random_d3(random_d3)
-    {}
+    const std::vector<T>& vec() const { return m_vec; }
 
-    // must accumulate all blocks
-    void accumVector(const BlockVector<T>& aA,   // before cosetFFT()
-                     const BlockVector<T>& aB) { // before cosetFFT()
-#ifdef USE_ASSERT
-        assert(aA.space() == aB.space() &&
-               aA.block() == aB.block());
-#endif
-
-        for (std::size_t i = aA.startIndex(); i < aA.stopIndex(); ++i) {
-            m_vec[i] = m_random_d2 * aA[i] + m_random_d1 * aB[i];
-        }
-
-        if (aA.space().blockID()[0] == ++m_blockCount) {
-            m_vec[0] -= m_random_d3;
-            m_qap.FFT()->add_poly_Z(m_random_d1 * m_random_d2, m_vec);
-        }
-    }
-
-    ////////////////////////////////////////
+private:
     // temporary H
-    //
-
-    QAP_WitnessH(const QAP_SystemPoint<SYS, T>& qap,
-                 const QAP_WitnessA<SYS, T>& aA, // after cosetFFT()
-                 const QAP_WitnessB<SYS, T>& aB, // after cosetFFT()
-                 const QAP_WitnessC<SYS, T>& aC) // after cosetFFT()
-        : QAP_WitnessH{qap}
+    QAP_WitnessABCH(const QAP_SystemPoint<SYS, T>& qap,
+                    const QAP_WitnessABC<SYS, T>& ABC) // after cosetFFT()
+        : m_vec(qap.degree(), T::zero())
     {
         for (std::size_t i = 0; i < m_vec.size(); ++i)
-            m_vec[i] = aA.vec()[i] * aB.vec()[i] - aC.vec()[i];
+            m_vec[i] =
+                ABC.vecA()[i] * ABC.vecB()[i] -
+                ABC.vecC()[i];
 
         qap.FFT()->divide_by_Z_on_coset(m_vec);
-        qap.FFT()->icosetFFT(m_vec, T::params.multiplicative_generator());
-    }
 
-    // use with accumVector() to avoid having all vectors in memory
-    QAP_WitnessH(const QAP_SystemPoint<SYS, T>& qap)
-        : m_vec(qap.degree(), T::zero()),
-          m_blockCount(0),
-          m_qap(qap),
-          m_random_d1(T::zero()), // not used
-          m_random_d2(T::zero()), // not used
-          m_random_d3(T::zero())  // not used
-    {}
-
-    // must accumulate all blocks
-    void accumVector(const BlockVector<T>& aA,   // after cosetFFT()
-                     const BlockVector<T>& aB,   // after cosetFFT()
-                     const BlockVector<T>& aC) { // after cosetFFT()
-#ifdef USE_ASSERT
-        assert(aA.space() == aB.space() &&
-               aB.space() == aC.space() &&
-               aA.block() == aB.block() &&
-               aB.block() == aC.block());
-#endif
-
-        for (std::size_t i = aA.startIndex(); i < aA.stopIndex(); ++i) {
-            m_vec[i] = aA[i] * aB[i] - aC[i];
-        }
-
-        if (aA.space().blockID()[0] == ++m_blockCount) {
-            m_qap.FFT()->divide_by_Z_on_coset(m_vec);
-            m_qap.FFT()->icosetFFT(m_vec, T::params.multiplicative_generator());
-        }
+        qap.FFT()->icosetFFT(m_vec,
+                             T::params.multiplicative_generator());
     }
 
     // add regular and temporary H together
-    void addTemporary(const QAP_WitnessH& tmpH) {
+    void addTemporary(const QAP_WitnessABCH& tmpH) {
 #ifdef USE_ASSERT
         // make sure to add temporary H, not regular H
         assert(tmpH.vec().size() < m_vec.size());
@@ -198,13 +143,7 @@ public:
             m_vec[i] += tmpH.vec()[i];
     }
 
-    const std::vector<T>& vec() const { return m_vec; }
-
-private:
     std::vector<T> m_vec;
-    std::size_t m_blockCount;
-    const QAP_SystemPoint<SYS, T>& m_qap;
-    const T m_random_d1, m_random_d2, m_random_d3;
 };
 
 } // namespace snarklib
