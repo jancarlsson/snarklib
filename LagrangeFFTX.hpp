ae4ca6621e400837ae0ba06ddb1f847907a1ed52
jancarlsson <jancarlsson@Safe-mail.net>
2015-03-08 22:37:37 +0000

    back out "prepare for blinded FFT entropy"

--- LagrangeFFTX.hpp
+++ LagrangeFFTX.hpp
@@ -9,10 +9,10 @@ namespace snarklib {
 // basic radix-2 evaluation domain
 //
 
-template <typename FR, typename BLIND>
-class basic_radix2_domain : public LagrangeFFT<FR, BLIND>::Base
+template <typename T>
+class basic_radix2_domain : public LagrangeFFT<T>::Base
 {
-    typedef typename LagrangeFFT<FR, BLIND>::Base BASE;
+    typedef typename LagrangeFFT<T>::Base BASE;
 
 public:
     basic_radix2_domain(const std::size_t min_size)
@@ -21,63 +21,61 @@ public:
     {
 #ifdef USE_ASSERT
         assert(min_size > 1);
-        assert(ceil_log2(min_size) <= FR::params.s());
+        assert(ceil_log2(min_size) <= T::params.s());
 #endif
     }
 
-    std::vector<BLIND> lagrange_coeffs(const std::vector<BLIND>& t) const {
-        return BASE::basic_radix2_lagrange_coeffs(BASE::min_size(), t[1]);
+    std::vector<T> lagrange_coeffs(const T& t) const {
+        return BASE::basic_radix2_lagrange_coeffs(BASE::min_size(), t);
     }
 
-    FR get_element(const std::size_t idx) const {
+    T get_element(const std::size_t idx) const {
         return omega ^ idx;
     }
 
-    BLIND compute_Z(const std::vector<BLIND>& t) const {
-        return
-            (t[1] ^ BASE::min_size())
-            - BLIND::one();
+    T compute_Z(const T& t) const {
+        return (t ^ BASE::min_size()) - T::one();
     }
 
-    void divide_by_Z_on_coset(std::vector<FR>& P) const {
-        const FR coset = FR::params.multiplicative_generator();
-        const FR Z_inverse_at_coset = inverse(BASE::compute_Z(coset));
+    void divide_by_Z_on_coset(std::vector<T>& P) const {
+        const T coset = T::params.multiplicative_generator();
+        const T Z_inverse_at_coset = inverse(compute_Z(coset));
         for (std::size_t i = 0; i < BASE::min_size(); ++i) {
             P[i] *= Z_inverse_at_coset;
         }
     }
 
 protected:
-    void m_FFT(std::vector<FR>& a) const {
+    void m_FFT(std::vector<T>& a) const {
         BASE::basic_radix2_FFT(a, omega);
     }
 
-    void m_iFFT(std::vector<FR>& a) const {
+    void m_iFFT(std::vector<T>& a) const {
         BASE::basic_radix2_FFT(a, inverse(omega));
 
-        const FR sconst = inverse(FR(a.size()));
+        const T sconst = inverse(T(a.size()));
         for (std::size_t i = 0; i < a.size(); ++i) {
             a[i] *= sconst;
         }
     }
 
-    void m_add_poly_Z(const FR& coeff, std::vector<FR>& H) const {
+    void m_add_poly_Z(const T& coeff, std::vector<T>& H) const {
         H[BASE::min_size()] += coeff;
         H[0] -= coeff;
     }
 
 private:
-    FR omega;
+    T omega;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // extended radix-2 evaluation domain
 //
 
-template <typename FR, typename BLIND>
-class extended_radix2_domain : public LagrangeFFT<FR, BLIND>::Base
+template <typename T>
+class extended_radix2_domain : public LagrangeFFT<T>::Base
 {
-    typedef typename LagrangeFFT<FR, BLIND>::Base BASE;
+    typedef typename LagrangeFFT<T>::Base BASE;
 
 public:
     extended_radix2_domain(const std::size_t min_size)
@@ -88,26 +86,26 @@ public:
     {
 #ifdef USE_ASSERT
         assert(min_size > 1);
-        assert(ceil_log2(min_size) == FR::params.s() + 1);
+        assert(ceil_log2(min_size) == T::params.s() + 1);
 #endif
     }
 
-    std::vector<BLIND> lagrange_coeffs(const std::vector<BLIND>& t) const {
+    std::vector<T> lagrange_coeffs(const T& t) const {
         const auto
-            T0 = BASE::basic_radix2_lagrange_coeffs(small_m, t[1]),
-            T1 = BASE::basic_radix2_lagrange_coeffs(small_m, t[1] * inverse(shift));
+            T0 = BASE::basic_radix2_lagrange_coeffs(small_m, t),
+            T1 = BASE::basic_radix2_lagrange_coeffs(small_m, t * inverse(shift));
 
-        std::vector<BLIND> result(BASE::min_size(), BLIND::zero());
+        std::vector<T> result(BASE::min_size(), T::zero());
 
-        const BLIND
-            t_to_small_m = t[1] ^ small_m,
+        const T
+            t_to_small_m = t ^ small_m,
             shift_to_small_m = shift ^ small_m;
 
-        const FR one_over_denom = inverse(shift_to_small_m - FR::one());
+        const T one_over_denom = inverse(shift_to_small_m - T::one());
 
-        const FR
+        const T
             T0_coeff = (t_to_small_m - shift_to_small_m) * (-one_over_denom),
-            T1_coeff = (t_to_small_m - BLIND::one()) * one_over_denom;
+            T1_coeff = (t_to_small_m - T::one()) * one_over_denom;
 
         for (std::size_t i = 0; i < small_m; ++i) {
             result[i] = T0[i] * T0_coeff;
@@ -117,33 +115,30 @@ public:
         return result;
     }
 
-    FR get_element(const std::size_t idx) const {
+    T get_element(const std::size_t idx) const {
         return (idx < small_m)
             ? omega ^ idx
             : shift * (omega ^ (idx - small_m));
     }
 
-    BLIND compute_Z(const std::vector<BLIND>& t) const {
-        const auto a = t[1] ^ small_m;
-
-        return
-            (a - BLIND::one())
-            * (a - (shift ^ small_m));
+    T compute_Z(const T& t) const {
+        const auto a = t ^ small_m;
+        return (a - T::one()) * (a - (shift ^ small_m));
     }
 
-    void divide_by_Z_on_coset(std::vector<FR>& P) const {
-        const FR coset = FR::params.multiplicative_generator();
+    void divide_by_Z_on_coset(std::vector<T>& P) const {
+        const T coset = T::params.multiplicative_generator();
 
-        const FR
+        const T
             coset_to_small_m = coset ^ small_m,
             shift_to_small_m = shift ^ small_m;
 
-        const FR
-            Z0 = (coset_to_small_m - FR::one()) * (coset_to_small_m - shift_to_small_m),
-            Z1 = (coset_to_small_m * shift_to_small_m - FR::one())
+        const T
+            Z0 = (coset_to_small_m - T::one()) * (coset_to_small_m - shift_to_small_m),
+            Z1 = (coset_to_small_m * shift_to_small_m - T::one())
                * (coset_to_small_m * shift_to_small_m - shift_to_small_m);
 
-        const FR
+        const T
             Z0_inverse = inverse(Z0),
             Z1_inverse = inverse(Z1);
 
@@ -154,14 +149,14 @@ public:
     }
 
 protected:
-    void m_FFT(std::vector<FR>& a) const {
-        std::vector<FR>
-            a0(small_m, FR::zero()),
-            a1(small_m, FR::zero());
+    void m_FFT(std::vector<T>& a) const {
+        std::vector<T>
+            a0(small_m, T::zero()),
+            a1(small_m, T::zero());
 
-        const FR shift_to_small_m = shift ^ small_m;
+        const T shift_to_small_m = shift ^ small_m;
 
-        FR shift_i = FR::one();
+        T shift_i = T::one();
         for (std::size_t i = 0; i < small_m; ++i) {
             a0[i] = a[i] + a[small_m + i];
             a1[i] = shift_i * (a[i] + shift_to_small_m * a[small_m + i]);
@@ -178,20 +173,20 @@ protected:
         }
     }
 
-    void m_iFFT(std::vector<FR>& a) const {
-        std::vector<FR>
+    void m_iFFT(std::vector<T>& a) const {
+        std::vector<T>
             a0(a.begin(), a.begin() + small_m),
             a1(a.begin() + small_m, a.end());
 
-        const FR omega_inverse = inverse(omega);
+        const T omega_inverse = inverse(omega);
         BASE::basic_radix2_FFT(a0, omega_inverse);
         BASE::basic_radix2_FFT(a1, omega_inverse);
 
-        const FR shift_to_small_m = shift ^ small_m;
-        const FR sconst = inverse(FR(small_m) * (FR::one() - shift_to_small_m));
+        const T shift_to_small_m = shift ^ small_m;
+        const T sconst = inverse(T(small_m) * (T::one() - shift_to_small_m));
 
-        const FR shift_inverse = inverse(shift);
-        FR shift_inverse_i = FR::one();
+        const T shift_inverse = inverse(shift);
+        T shift_inverse_i = T::one();
 
         for (std::size_t i = 0; i < small_m; ++i) {
             a[i] = sconst * (-shift_to_small_m * a0[i] + shift_inverse_i * a1[i]);
@@ -201,27 +196,27 @@ protected:
         }
     }
 
-    void m_add_poly_Z(const FR& coeff, std::vector<FR>& H) const {
-        const FR shift_to_small_m = shift ^ small_m;
+    void m_add_poly_Z(const T& coeff, std::vector<T>& H) const {
+        const T shift_to_small_m = shift ^ small_m;
 
         H[BASE::min_size()] += coeff;
-        H[small_m] -= coeff * (shift_to_small_m + FR::one());
+        H[small_m] -= coeff * (shift_to_small_m + T::one());
         H[0] += coeff * shift_to_small_m;
     }
 
 private:
     std::size_t small_m;
-    FR omega, shift;
+    T omega, shift;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // step radix-2 evaluation domain
 //
 
-template <typename FR, typename BLIND>
-class step_radix2_domain : public LagrangeFFT<FR, BLIND>::Base
+template <typename T>
+class step_radix2_domain : public LagrangeFFT<T>::Base
 {
-    typedef typename LagrangeFFT<FR, BLIND>::Base BASE;
+    typedef typename LagrangeFFT<T>::Base BASE;
 
 public:
     step_radix2_domain(const std::size_t min_size)
@@ -238,26 +233,25 @@ public:
 #endif
     }
 
-    std::vector<BLIND> lagrange_coeffs(const std::vector<BLIND>& t) const {
+    std::vector<T> lagrange_coeffs(const T& t) const {
         const auto
-            inner_big = BASE::basic_radix2_lagrange_coeffs(big_m, t[1]),
-            inner_small = BASE::basic_radix2_lagrange_coeffs(small_m, t[1] * inverse(omega));
+            inner_big = BASE::basic_radix2_lagrange_coeffs(big_m, t),
+            inner_small = BASE::basic_radix2_lagrange_coeffs(small_m, t * inverse(omega));
 
-        std::vector<BLIND> result(BASE::min_size(), BLIND::zero());
+        std::vector<T> result(BASE::min_size(), T::zero());
 
-        const BLIND
-            L0 = (t[1] ^ small_m) - (omega ^ small_m),
+        const T
+            L0 = (t ^ small_m) - (omega ^ small_m),
             omega_to_small_m = omega ^ small_m,
             big_omega_to_small_m = big_omega ^ small_m;
 
-        FR elt = FR::one();
+        T elt = T::one();
         for (std::size_t i = 0; i < big_m; ++i) {
             result[i] = inner_big[i] * L0 * inverse(elt - omega_to_small_m);
             elt *= big_omega_to_small_m;
         }
 
-        const BLIND L1 = ((t[1] ^ big_m) - BLIND::one())
-                       * inverse((omega ^ big_m) - BLIND::one());
+        const T L1 = ((t ^ big_m) - T::one()) * inverse((omega ^ big_m) - T::one());
 
         for (std::size_t i = 0; i < small_m; ++i) {
             result[big_m + i] = L1 * inner_small[i];
@@ -266,37 +260,35 @@ public:
         return result;
     }
 
-    FR get_element(const std::size_t idx) const {
+    T get_element(const std::size_t idx) const {
         return (idx < big_m)
             ? big_omega ^ idx
             : omega * (small_omega ^ (idx - big_m));
     }
 
-    BLIND compute_Z(const std::vector<BLIND>& t) const {
-        return
-            ((t[1] ^ big_m) - BLIND::one())
-            * ((t[1] ^ small_m) - (omega ^ small_m));
+    T compute_Z(const T& t) const {
+        return ((t ^ big_m) - T::one()) * ((t ^ small_m) - (omega ^ small_m));
     }
 
-    void divide_by_Z_on_coset(std::vector<FR>& P) const {
-        const FR coset = FR::params.multiplicative_generator();
-        const FR Z0 = (coset ^ big_m) - FR::one();
+    void divide_by_Z_on_coset(std::vector<T>& P) const {
+        const T coset = T::params.multiplicative_generator();
+        const T Z0 = (coset ^ big_m) - T::one();
 
-        const FR
+        const T
             coset_to_small_m_times_Z0 = (coset ^ small_m) * Z0,
             omega_to_small_m_times_Z0 = (omega ^ small_m) * Z0,
             omega_to_2small_m = omega ^ (2 * small_m);
 
-        FR elt = FR::one();
+        T elt = T::one();
         for (std::size_t i = 0; i < big_m; ++i) {
             P[i] *= inverse(coset_to_small_m_times_Z0 * elt - omega_to_small_m_times_Z0);
             elt *= omega_to_2small_m;
         }
 
-        const FR Z1 = (((coset * omega) ^ big_m) - FR::one())
-                    * (((coset * omega) ^ small_m) - (omega ^ small_m));
+        const T Z1 = (((coset * omega) ^ big_m) - T::one())
+                   * (((coset * omega) ^ small_m) - (omega ^ small_m));
 
-        const FR Z1_inverse = inverse(Z1);
+        const T Z1_inverse = inverse(Z1);
 
         for (std::size_t i = 0; i < small_m; ++i) {
             P[big_m + i] *= Z1_inverse;
@@ -304,12 +296,12 @@ public:
     }
 
 protected:
-    void m_FFT(std::vector<FR>& a) const {
-        std::vector<FR>
-            c(big_m, FR::zero()),
-            d(big_m, FR::zero());
+    void m_FFT(std::vector<T>& a) const {
+        std::vector<T>
+            c(big_m, T::zero()),
+            d(big_m, T::zero());
 
-        FR omega_i = FR::one();
+        T omega_i = T::one();
         for (std::size_t i = 0; i < big_m; ++i) {
             if (i < small_m) {
                 c[i] = a[i] + a[i + big_m];
@@ -322,7 +314,7 @@ protected:
             omega_i *= omega;
         }
 
-        std::vector<FR> e(small_m, FR::zero());
+        std::vector<T> e(small_m, T::zero());
 
         const std::size_t compr = 1u << (ceil_log2(big_m) - ceil_log2(small_m));
         for (std::size_t i = 0; i < small_m; ++i) {
@@ -342,26 +334,26 @@ protected:
         }
     }
 
-    void m_iFFT(std::vector<FR>& a) const {
-        std::vector<FR>
+    void m_iFFT(std::vector<T>& a) const {
+        std::vector<T>
             U0(a.begin(), a.begin() + big_m),
             U1(a.begin() + big_m, a.end());
 
         BASE::basic_radix2_FFT(U0, inverse(squared(omega)));
         BASE::basic_radix2_FFT(U1, inverse(BASE::get_root_of_unity(small_m)));
 
-        const FR U0_size_inv = inverse(FR(big_m));
+        const T U0_size_inv = inverse(T(big_m));
         for (std::size_t i = 0; i < big_m; ++i) {
             U0[i] *= U0_size_inv;
         }
 
-        const FR U1_size_inv = inverse(FR(small_m));
+        const T U1_size_inv = inverse(T(small_m));
         for (std::size_t i = 0; i < small_m; ++i) {
             U1[i] *= U1_size_inv;
         }
 
-        std::vector<FR> tmp = U0;
-        FR omega_i = FR::one();
+        std::vector<T> tmp = U0;
+        T omega_i = T::one();
         for (std::size_t i = 0; i < big_m; ++i) {
             tmp[i] *= omega_i;
             omega_i *= omega;
@@ -378,22 +370,22 @@ protected:
             }
         }
 
-        const FR omega_inv = inverse(omega);
-        FR omega_inv_i = FR::one();
+        const T omega_inv = inverse(omega);
+        T omega_inv_i = T::one();
         for (std::size_t i = 0; i < small_m; ++i) {
             U1[i] *= omega_inv_i;
             omega_inv_i *= omega_inv;
         }
 
-        const FR over_two = inverse(FR(2ul));
+        const T over_two = inverse(T(2ul));
         for (std::size_t i = 0; i < small_m; ++i) {
             a[i] = (U0[i] + U1[i]) * over_two;
             a[big_m + i] = (U0[i] - U1[i]) * over_two;
         }
     }
 
-    void m_add_poly_Z(const FR& coeff, std::vector<FR>& H) const {
-        const FR omega_to_small_m = omega ^ small_m;
+    void m_add_poly_Z(const T& coeff, std::vector<T>& H) const {
+        const T omega_to_small_m = omega ^ small_m;
 
         H[BASE::min_size()] += coeff;
         H[big_m] -= coeff * omega_to_small_m;
@@ -403,30 +395,30 @@ protected:
 
 private:
     std::size_t big_m, small_m;
-    FR omega, big_omega, small_omega;
+    T omega, big_omega, small_omega;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // constructor factory function
 //
 
-template <typename FR, typename BLIND>
+template <typename T>
 void* get_evaluation_domain(const std::size_t min_size) {
-    typename LagrangeFFT<FR, BLIND>::Base* ptr = nullptr;
+    typename LagrangeFFT<T>::Base* ptr = nullptr;
 
 #ifdef USE_ASSERT
     assert(min_size > 1);
 #endif
     const std::size_t log_min_size = ceil_log2(min_size);
 #ifdef USE_ASSERT
-    assert(log_min_size <= (FR::params.s() + 1));
+    assert(log_min_size <= (T::params.s() + 1));
 #endif
 
     if (min_size == (1u << log_min_size)) {
-        if (log_min_size == FR::params.s() + 1) {
-            ptr = new extended_radix2_domain<FR, BLIND>(min_size);
+        if (log_min_size == T::params.s() + 1) {
+            ptr = new extended_radix2_domain<T>(min_size);
         } else {
-            ptr = new basic_radix2_domain<FR, BLIND>(min_size);
+            ptr = new basic_radix2_domain<T>(min_size);
         }
     } else {
         const std::size_t big = 1u << (ceil_log2(min_size) - 1);
@@ -434,13 +426,13 @@ void* get_evaluation_domain(const std::size_t min_size) {
         const std::size_t rounded_small = 1u << ceil_log2(small);
 
         if (big == rounded_small) {
-            if (ceil_log2(big + rounded_small) < FR::params.s() + 1) {
-                ptr = new basic_radix2_domain<FR, BLIND>(big + rounded_small);
+            if (ceil_log2(big + rounded_small) < T::params.s() + 1) {
+                ptr = new basic_radix2_domain<T>(big + rounded_small);
             } else {
-                ptr = new extended_radix2_domain<FR, BLIND>(big + rounded_small);
+                ptr = new extended_radix2_domain<T>(big + rounded_small);
             }
         } else {
-            ptr = new step_radix2_domain<FR, BLIND>(big + rounded_small);
+            ptr = new step_radix2_domain<T>(big + rounded_small);
         }
     }
 
