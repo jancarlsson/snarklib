dfadb72b9cb837caf03c4387bf4d28c74152191c
jancarlsson <jancarlsson@Safe-mail.net>
2015-05-11 19:37:22 +0000

    scope header file paths

--- EC_Edwards_InitGroups.hpp
+++ EC_Edwards_InitGroups.hpp
@@ -1,9 +1,9 @@
 #ifndef _SNARKLIB_EC_EDWARDS_INIT_GROUPS_HPP_
 #define _SNARKLIB_EC_EDWARDS_INIT_GROUPS_HPP_
 
-#include "EC.hpp"
-#include "EC_Edwards_GroupCurve.hpp"
-#include "Group.hpp"
+#include <snarklib/EC.hpp>
+#include <snarklib/EC_Edwards_GroupCurve.hpp>
+#include <snarklib/Group.hpp>
 
 namespace snarklib {
 
